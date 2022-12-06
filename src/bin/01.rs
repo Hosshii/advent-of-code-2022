ec3ca622f1bbe62b7ec1ffbfc43b03f6865fb6dd
Hosshii <sao_heath6147.wistre@icloud.com>
2022-12-07 00:37:15 +0900

    day01.

---
+++ src/bin/01.rs
@@ -0,0 +1,39 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    input
+        .split("\n\n")
+        .map(|v| v.lines().map(|v| v.parse::<u32>().unwrap()).sum())
+        .max()
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut sums: Vec<u32> = input
+        .split("\n\n")
+        .map(|v| v.lines().map(|v| v.parse::<u32>().unwrap()).sum())
+        .collect();
+
+    sums.sort_by(|a, b| b.cmp(a));
+    Some(sums.into_iter().take(3).sum())
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input), Some(24000));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), Some(45000));
+    }
+}
